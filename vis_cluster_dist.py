0bdb7a8e7aff1f59d2c7f7481685686fefd38806
Joshua Wu <wu.joshuah@gmail.com>
2024-02-14 13:49:22 -0500

    created new branch for testing invariant dim vae with scrubber

--- vis_cluster_dist.py
+++ vis_cluster_dist.py
@@ -1,10 +1,10 @@
 import ssumo
-from torch.utils.data import DataLoader
 from dappy import read
 import torch
 from dappy import visualization as vis
 import numpy as np
 from pathlib import Path
+from scipy.stats import circvar
 
 from base_path import RESULTS_PATH
 
@@ -15,19 +15,17 @@ config["model"]["load_model"] = config["out_path"]
 config["model"]["start_epoch"] = 300
 
 dataset_label = "Train"
-dataset = ssumo.data.get_mouse(
+dataset, loader = ssumo.data.get_mouse(
     data_config=config["data"],
     window=config["model"]["window"],
     train=dataset_label == "Train",
     data_keys=["x6d", "root", "offsets", "raw_pose"]
     + config["disentangle"]["features"],
-)
-loader = DataLoader(
-    dataset=dataset, batch_size=config["train"]["batch_size"], shuffle=False
+    shuffle=False,
 )
 k_pred = np.load(vis_path + "z_gmm.npy")
 
-for key in ["heading", "avg_speed"]:
+for key in ["heading"]:
     k_pred_null = np.load(vis_path + "z_{}_gmm.npy".format(key))
     if key == "heading":
         heading = dataset[:]["heading"].cpu().detach().numpy()
@@ -35,14 +33,24 @@ for key in ["heading", "avg_speed"]:
     else:
         feat = dataset[:][key].cpu().detach().numpy().squeeze()
 
+    import pdb; pdb.set_trace()
+
+    z_cvar, z_null_cvar = [], []
+    for i in range(25):
+        z_cvar += [circvar(feat[k_pred == i], high=np.pi, low=-np.pi)]
+        z_null_cvar += [circvar(feat[k_pred_null == i], high=np.pi, low=-np.pi)]
+
+    print(np.mean(z_cvar))
+    print(np.mean(z_null_cvar))
+
     ssumo.plot.feature_ridge(
         feature=feat,
         labels=k_pred,
         xlabel=key,
         ylabel="Cluster",
-        x_lim = (feat.min() - 0.1, feat.max() + 0.1),
+        x_lim=(feat.min() - 0.1, feat.max() + 0.1),
         n_bins=200,
-        binrange = (feat.min(), feat.max()),
+        binrange=(feat.min(), feat.max()),
         path="{}{}_".format(vis_path, key),
     )
 
@@ -51,8 +59,8 @@ for key in ["heading", "avg_speed"]:
         labels=k_pred_null,
         xlabel=key,
         ylabel="Cluster",
-        x_lim = (feat.min() - 0.1, feat.max() + 0.1),
-        binrange = (feat.min(), feat.max()),
+        x_lim=(feat.min() - 0.1, feat.max() + 0.1),
+        binrange=(feat.min(), feat.max()),
         n_bins=200,
         path="{}{}_null_".format(vis_path, key),
     )
