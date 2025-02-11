fixed issue with normalizing speed variable

scripts/prepare_save_data.py
@@ -5,6 +5,8 @@ from typing import List
 import torch
 from scrubvae.data.dataset import *
 import h5py
+from neuroposelib import vis
+
 
 def calculate_mouse_kinematics(
     data_path: dict,
@@ -17,22 +19,16 @@ def calculate_mouse_kinematics(
     direction_process: str = "midfwd",
 ):
     print("Calculating dataset: {} {}".format(dataset, train_val_test))
-    pose = np.load(
-        "{}{}/{}/pose.npy".format(
-            data_path, dataset, train_val_test
-        )
-    )
+    pose = np.load("{}{}/{}/pose.npy".format(data_path, dataset, train_val_test))
     ids = np.repeat(np.arange(4, dtype=int), pose.shape[0] // 4)
 
     # Filter out bad tracking using speed threshold
     if remove_speed_outliers is not None:
-        outlier_frames = get_speed_outliers(
-            pose, remove_speed_outliers
-        )
+        outlier_frames = get_speed_outliers(pose, remove_speed_outliers)
         pose = np.delete(pose, outlier_frames, 0)
         ids = np.delete(ids, outlier_frames, 0)
 
-    data = {"raw_pose": pose} if "raw_pose" in data_keys else {}
+    data = {"raw_pose": pose}
     # speed_keys = [key for key in data_keys if "speed" in key]
     # assert len(speed_keys) < 2
     # Calculate different speed representations
@@ -53,22 +49,24 @@ def calculate_mouse_kinematics(
         #     speed = np.sqrt((speed**2).sum(axis=-1)).mean(axis=(-1, -2))
 
     # Get yaw of mid-spine -> fwd-spine segment for central frame in all windows
-    yaw = get_frame_yaw(pose[:, window//2, ...], 0, 1)[..., None]
+    yaw = get_frame_yaw(pose[:, window // 2, ...], 0, 1)[..., None]
 
-    # if "heading" in data_keys:
-    data["heading"] = get_angle2D(yaw)
+    if "heading" in data_keys:
+        data["heading"] = get_angle2D(yaw)
 
     # # Get yaw of center frame in a window
     # if direction_process in ["midfwd", "x360"]:
     #     yaw = yaw[window_inds][:, window // 2]
 
     if ("root" or "x6d") in data_keys:
-        root = pose[..., 0, :] # (n_samples, window, 3)
+        root = pose[..., 0, :].copy()  # (n_samples, window, 3)
         if direction_process in ["midfwd", "x360"]:
             # Centering root of middle frame
             # root = pose[..., 0, :][window_inds]
-            root_center = np.zeros(root.shape) # (n_samples, window, 3)
-            root_center[..., [0, 1]] = root[:, window // 2, [0, 1]][:, None, :] # (n_samples, 1, 2)
+            root_center = np.zeros(root.shape)  # (n_samples, window, 3)
+            root_center[..., [0, 1]] = root[:, window // 2, [0, 1]][
+                :, None, :
+            ]  # (n_samples, 1, 2)
 
             root -= root_center
         # elif direction_process == "fwd":
@@ -87,7 +85,7 @@ def calculate_mouse_kinematics(
 
         if direction_process == "midfwd":
             # if "mid" in direction_process:
-                ## Center frame of a window is translated to center and rotated to x+
+            ## Center frame of a window is translated to center and rotated to x+
             fwd_qtn = np.zeros((len(yaw), 4))
             fwd_qtn[:, [-1, 0]] = get_angle2D(yaw / 2)
             # local_qtn = local_qtn[window_inds]
@@ -125,7 +123,8 @@ def calculate_mouse_kinematics(
     data = {k: torch.tensor(v, dtype=torch.float32) for k, v in data.items()}
 
     # Get animal IDs
-    data["ids"] = torch.tensor(ids, dtype=torch.int16)
+    if "ids" in data_keys:
+        data["ids"] = torch.tensor(ids, dtype=torch.int16)
 
     # if "fluorescence" in data_keys:
     #     # Read in integrated fluorescence for PD dataset
@@ -137,13 +136,41 @@ def calculate_mouse_kinematics(
     #     fluorescence = meta_by_frame["Fluorescence"].to_numpy()
     #     data["fluorescence"] = torch.tensor(fluorescence, dtype=torch.float32)
 
+    sample_ind = [105000, 1000, 2000, 200000]
+    test_pose = fwd_kin_cont6d_torch(
+        data["x6d"][sample_ind].reshape(-1, 18, 6),
+        skeleton_config["KINEMATIC_TREE"],
+        data["offsets"][sample_ind].reshape(-1, 18, 3),
+        root_pos=data["root"][sample_ind].reshape(-1, 3),
+        do_root_R=True,
+        eps=1e-8,
+    )
+
+    test_pose = np.concatenate([data["raw_pose"][sample_ind].reshape(-1, 18, 3),test_pose], axis=0)
+
+    connectivity = read.connectivity_config(data_path + "/mouse_skeleton.yaml")
+    vis.pose.grid3D(
+        test_pose,
+        connectivity,
+        frames=np.arange(2)*51*len(sample_ind),
+        centered=False,
+        subtitles=["Raw", "Target"],
+        title=label + " Data",
+        fps=45,
+        figsize=(36, 12),
+        N_FRAMES= window*len(sample_ind),
+        VID_NAME="{}_{}.mp4".format(direction_process, train_val_test),
+        SAVE_ROOT="./",
+    )
+
+    # import pdb; pdb.set_trace()
+
     if "target_pose" in data_keys:
         # Target pose root does not move
         reshaped_x6d = data["x6d"].reshape((-1,) + data["x6d"].shape[-2:])
         # if direction_process == "midfwd":
-        offsets = data["offsets"].reshape(
-            reshaped_x6d.shape[:2] + (-1,)
-        )
+        offsets = data["offsets"].reshape(reshaped_x6d.shape[:2] + (-1,))
+
         # else:
         #     offsets = data["offsets"]
 
@@ -157,10 +184,8 @@ def calculate_mouse_kinematics(
         ).reshape(data["x6d"].shape[:-1] + (3,))
 
     for k, v in data.items():
-        full_path = "{}{}/{}/{}".format(
-            data_path, dataset, train_val_test, k
-        )
-        if k in ["ids", "heading", "avg_speed_3d", "offsets"]:
+        full_path = "{}{}/{}/{}".format(data_path, dataset, train_val_test, k)
+        if k in ["ids", "heading", "avg_speed_3d", "offsets", "raw_pose"]:
             full_path += ".h5"
         else:
             full_path += "_{}.h5".format(direction_process)
@@ -176,7 +201,7 @@ def calculate_mouse_kinematics(
 
 
 data_path = "/mnt/home/jwu10/working/ceph/data/wu_iclr25/"
-data_keys = ["x6d", "root", "offsets", "target_pose", "avg_speed_3d", "heading", "ids"]
+data_keys = ["raw_pose", "x6d", "root", "offsets", "target_pose", "avg_speed_3d", "heading", "ids"]
 skeleton_config = read.config(data_path + "mouse_skeleton.yaml")
 
 for label in ["train", "val", "test"]:

scripts/vis_reconstruction.py
@@ -19,7 +19,7 @@ def visualize_reconstruction(model, loader, label, connectivity):
         data_o = scrubvae.train.predict_batch(
             model, data, disentangle_keys=config["disentangle"]["features"]
         )
-
+        # import pdb; pdb.set_trace()
         pose_hat = fwd_kin_cont6d_torch(
             data_o["x6d"].reshape(-1, n_keypts, 6),
             kinematic_tree,
@@ -28,10 +28,18 @@ def visualize_reconstruction(model, loader, label, connectivity):
             do_root_R=True,
         )
 
+        target_pose = fwd_kin_cont6d_torch(
+            data["x6d"].reshape(-1, n_keypts, 6),
+            kinematic_tree,
+            data["offsets"].view(-1, n_keypts, 3),
+            data["root"].reshape(-1, 3),
+            do_root_R=True,
+        )
+
         pose_array = torch.cat(
             [
                 data["raw_pose"].reshape(-1, n_keypts, 3),
-                data["target_pose"].reshape(-1, n_keypts, 3),
+                target_pose.reshape(-1, n_keypts, 3),
                 pose_hat,
             ],
             axis=0,
@@ -55,23 +63,22 @@ def visualize_reconstruction(model, loader, label, connectivity):
             SAVE_ROOT=config["out_path"],
         )
 
-
 analysis_key = sys.argv[1]
 config = read.config(RESULTS_PATH + analysis_key + "/model_config.yaml")
-config["data"]["stride"] = 10
 config["data"]["batch_size"] = 10
-connectivity = read.connectivity_config(config["data"]["skeleton_path"])
-dataset_list = ["Train", "Test"]
+connectivity = read.connectivity_config(config["data"]["data_path"] + "/mouse_skeleton.yaml")
+dataset_list = ["val", "test"]
 for dataset_label in dataset_list:
-    loader, model = scrubvae.get.data_and_model(
+    loader_dict, model = scrubvae.get.data_and_model(
         config,
         load_model=config["out_path"],
         epoch=sys.argv[2],
-        dataset_label="Train",
-        data_keys=["x6d", "root", "offsets", "raw_pose", "target_pose"]
+        train_val_test=[dataset_label],
+        data_keys=["x6d", "root", "offsets", "raw_pose", "target_pose", "ids"]
         + config["disentangle"]["features"],
-        shuffle=True,
+        shuffle=[True],
+        use_default_val_keys = False,
         verbose=0,
     )
 
-    visualize_reconstruction(model, loader, dataset_label, connectivity)
+    visualize_reconstruction(model, loader_dict[dataset_label], dataset_label, connectivity)

src/scrubvae/get/data.py
@@ -16,6 +16,7 @@ TRAIN_IDS = {
     "neurips_mouse": np.arange(9),
 }
 
+
 def mouse_pd_data(
     data_config: dict,
     window: int = 51,
@@ -113,8 +114,8 @@ def mouse_data(
     train_val_test: str = "train",
     data_keys: List[str] = ["x6d", "root", "offsets"],
     shuffle: bool = False,
-    normalize: List[str] = [],
-    norm_params: dict = None,
+    # normalize: List[str] = [],
+    # norm_params: dict = None,
 ):
     """
     Load in mouse data and return pytorch dataloaders
@@ -147,7 +148,7 @@ def mouse_data(
     )
     data = {}
     for key in data_keys:
-        if key in ["ids", "heading", "avg_speed_3d", "offsets"]:
+        if key in ["ids", "heading", "avg_speed_3d", "offsets", "raw_pose"]:
             file_path = "{}{}.h5".format(data_path, key)
         else:
             file_path = "{}{}_{}.h5".format(
@@ -159,38 +160,50 @@ def mouse_data(
         hf.close()
 
     data = {k: torch.from_numpy(v) for k, v in data.items()}
-    if (norm_params is None) or (norm_params == {}):
-        norm_params = {}
+    # if (norm_params is None) or (norm_params == {}):
+    #     norm_params = {}
 
     # Normalize disentanglement variables if needed
-    for key in normalize:
-        if (key not in ["heading", "ids", "fluorescence", "pd_label"]) and (
-            key in data_keys
-        ):
-            if data_config["normalize"] == "bounded":
-                print(
-                    "Rescaling decoding variable, {}, to be between -1 and 1".format(
-                        key
-                    )
-                )
-                if key not in norm_params.keys():
-                    norm_params[key] = {}
-                    norm_params[key]["min"] = data[key].min(dim=0)[0] * 0.9
-                    norm_params[key]["max"] = data[key].max(dim=0)[0] * 1.1
-                data[key] -= norm_params[key]["min"]
-                data[key] = 2 * data[key] / norm_params[key]["max"] - 1
-                assert data[key].max() < 1
-                assert data[key].min() > -1
-            elif data_config["normalize"] == "z_score":
-                print(
-                    "Mean centering and unit standard deviation-scaling {}".format(key)
-                )
-                if key not in norm_params.keys():
-                    norm_params[key] = {}
-                    norm_params[key]["mean"] = data[key].mean(axis=0)
-                    norm_params[key]["std"] = data[key].std(axis=0)
-                data[key] -= norm_params[key]["mean"]
-                data[key] /= norm_params[key]["std"]
+    # for key in normalize:
+    #     if (key not in ["heading", "ids", "fluorescence", "pd_label"]) and (
+    #         key in data_keys
+    #     ):
+            # if data_config["normalize"] == "bounded":
+            #     print(
+            #         "Rescaling decoding variable, {}, to be between -1 and 1".format(
+            #             key
+            #         )
+            #     )
+            #     if key not in norm_params.keys():
+            #         norm_params[key] = {}
+            #         norm_params[key]["min"] = data[key].min(dim=0)[0] * 0.9
+            #         norm_params[key]["max"] = data[key].max(dim=0)[0] * 1.1
+            #     data[key] -= norm_params[key]["min"]
+            #     data[key] = 2 * data[key] / norm_params[key]["max"] - 1
+            #     assert data[key].max() < 1
+            #     assert data[key].min() > -1
+            # elif data_config["normalize"] == "z_score":
+    if "avg_speed_3d" in data_keys:
+        key = "avg_speed_3d"
+        norm_params = {
+            "avg_speed_3d": {
+                "mean": torch.tensor(
+                    [0.4993, 0.7112, 0.6663], dtype=torch.float32
+                ),
+                "std": torch.tensor(
+                    [0.4038, 0.3586, 0.4169], dtype=torch.float32
+                ),
+            }
+        }
+        print(
+            "Mean centering and unit standard deviation-scaling {}".format(key)
+        )
+        # if key not in norm_params.keys():
+        #     norm_params[key] = {}
+        #     norm_params[key]["mean"] = data[key].mean(axis=0)
+        #     norm_params[key]["std"] = data[key].std(axis=0)
+        data[key] -= norm_params[key]["mean"]
+        data[key] /= norm_params[key]["std"]
 
     discrete_classes = {}
     if data_config["dataset"] == "parkinson":