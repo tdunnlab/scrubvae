fixed segment_lens normalization

src/ssumo/get/data.py
@@ -556,26 +556,26 @@ def get_projected_2D_kinematics(
 
     if "offsets" in data_keys:
         segment_lens = get_segment_len_torch(
-            flattened_pose,
+            flattened_pose,  # flattened pose post-projection
             skeleton_config["KINEMATIC_TREE"],
             torch.tensor(skeleton_config["OFFSET"]).type(torch.float32),
             device=device,
         )
         segment_lens_3d = get_segment_len_torch(
-            data["raw_pose"].reshape((-1,) + data["raw_pose"].shape[-2:]),
+            data["raw_pose"].reshape(
+                (-1,) + data["raw_pose"].shape[-2:]
+            ),  # 3D flattened pose
             skeleton_config["KINEMATIC_TREE"],
             torch.tensor(skeleton_config["OFFSET"]).type(torch.float32),
             device=device,
         )
-        data["offsets"] = torch.reshape(segment_lens, pose.shape)
+        data["offsets"] = torch.reshape(segment_lens_3d, pose.shape)
         segment_lens_norm = torch.reshape(
-            torch.sum(torch.abs(segment_lens), axis=-1), pose.shape[:-1]
+            torch.sum(segment_lens, axis=-1), pose.shape[:-1]
         )
         segment_lens_norm = torch.nan_to_num(
             segment_lens_norm
-            / torch.reshape(
-                torch.sum(torch.abs(segment_lens_3d), axis=-1), pose.shape[:-1]
-            )
+            / torch.reshape(torch.sum(segment_lens_3d, axis=-1), pose.shape[:-1])
         )  # calculates length of each segment at each frame
         data["segment_lens"] = segment_lens_norm
 
@@ -673,7 +673,7 @@ def calculate_mouse_kinematics(
     if data_config["direction_process"] in ["midfwd", "x360"]:
         yaw = yaw[window_inds][:, window // 2]  # [..., None]
 
-    if ("root" or "x6d") in data_keys:
+    if "root" in data_keys or "x6d" in data_keys:
         root = pose[..., 0, :]
         if data_config["direction_process"] in ["midfwd", "x360"]:
             # Centering root
@@ -710,8 +710,8 @@ def calculate_mouse_kinematics(
             if "root" in data_keys:
                 root = qtn.qrot_np(fwd_qtn, root)
 
-            assert len(root) == len(window_inds)
-            assert len(local_qtn) == len(window_inds)
+            # assert len(root) == len(window_inds)
+            # assert len(local_qtn) == len(window_inds)
 
         data["x6d"] = qtn.quaternion_to_cont6d_np(local_qtn)
 

src/ssumo/train/losses.py
@@ -161,6 +161,7 @@ def mpjpe_loss(pose, x_hat, kinematic_tree, offsets, root=None, root_hat=None):
     # )
     # pose = x
     root_reshaped = root_hat.reshape((-1, 3))
+    x6d_in = x_hat.reshape((-1,) + x_hat.shape[-2:])
     if is_2D:
         local_ang = x_hat.reshape((-1,) + x_hat.shape[-2:])
         reshaped_x6d = torch.concatenate(
@@ -174,8 +175,9 @@ def mpjpe_loss(pose, x_hat, kinematic_tree, offsets, root=None, root_hat=None):
         root_reshaped = torch.concatenate(
             [root_reshaped, torch.zeros_like(root_reshaped[..., 0, None])], axis=-1
         )
+        x6d_in = reshaped_x6d
     pose_hat = fwd_kin_cont6d_torch(
-        reshaped_x6d,
+        x6d_in,
         kinematic_tree,
         offsets.reshape((-1,) + offsets.shape[-2:]),
         root_pos=root_reshaped,
@@ -223,7 +225,6 @@ def get_batch_loss(model, data, data_o, loss_scale, disentangle_config):
     if "jpe" in loss_scale.keys():
         offsets = data["offsets"]
         if "segment_lens" in data_o.keys():
-            offsets = torch.nan_to_num(data["offsets"] / abs(data["offsets"]))
             offsets = offsets * data_o["segment_lens"][..., None].repeat(1, 1, 1, 3)
         batch_loss["jpe"] = mpjpe_loss(
             data["target_pose"],  # data["x6d"].reshape(-1, *data["x6d"].shape[-2:]),

vis_reconstruction.py
@@ -1,11 +1,11 @@
 from ssumo.data.dataset import fwd_kin_cont6d_torch
 from ssumo.get.data import get_projected_2D_kinematics, get_random_axis
 from torch.utils.data import DataLoader
-from dappy import read
+from neuroposelib import read
 import torch
-from dappy import vis
+from neuroposelib import vis
 import ssumo
-from base_path import RESULTS_PATH
+from scripts.base_path import RESULTS_PATH
 import sys
 from math import pi
 
@@ -35,7 +35,10 @@ def visualize_2D_reconstruction(model, loader, label, connectivity, config):
         )
         data = {k: v.to("cuda") for k, v in data.items()}
         data_o = ssumo.train.predict_batch(
-            model, data, disentangle_keys=config["disentangle"]["features"]
+            model,
+            data,
+            disentangle_keys=config["disentangle"]["features"]
+            + ["segment_lens" for i in [1] if config["data"].get("segment_lens")],
         )
         x_hat = data_o["x6d"]
         local_ang = x_hat.reshape((-1,) + x_hat.shape[-2:])
@@ -145,7 +148,7 @@ for dataset_label in dataset_list:
         dataset_label="Train",
         data_keys=[
             "x6d",
-            "root",
+            # "root",
             "offsets",
             "raw_pose",
             "target_pose",